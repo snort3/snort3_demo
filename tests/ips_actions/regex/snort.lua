d7c8257f237a4cd4382f59cce66c125c65ec2844
russ <rucombs@cisco.com>
2020-06-09 08:01:38 -0400

    ips: updates for detection and regex

--- tests/ips_actions/regex/snort.lua
+++ tests/ips_actions/regex/snort.lua
@@ -5,7 +5,7 @@ stream = { }
 local_rules =
 [[
     alert tcp ( sid:2; msg:"regex"; 
-        regex:"^transfer-encoding\h*:\h*chunked\r$", nocase, multiline, fast_pattern; )
+        regex:"/^transfer-encoding\h*:\h*chunked\r$/im", fast_pattern; )
 ]]
 
 ips = { rules = local_rules }
