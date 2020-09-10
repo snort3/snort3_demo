7bcb6f3c84a7ee37cb7881c534ffce1be7ad187a
mdagon <mdagon@cisco.com>
2020-09-10 11:49:11 -0400

    http2_inspect: update readme, cleanup snort.lua

--- tests/inspectors/http2_inspect/snort.lua
+++ tests/inspectors/http2_inspect/snort.lua
@@ -17,10 +17,10 @@ http2_inspect = { }
 
 default_rules =
 [[
-alert tcp ( msg:"HTTP/2 http_uri match"; http_uri; content:"darkpig",fast_pattern; sid:1;)
+alert tcp ( msg:"http_uri match"; http_uri; content:"darkpig",fast_pattern; sid:1;)
 alert tcp ( msg:"status code match"; http_stat_code; content:"200"; sid:2; )
 alert tcp ( msg:"raw header match"; http_raw_header; content:"nginx/1.16.0"; sid:3; )
-alert tcp ( msg:"HTTP/2 fast pattern file data match in another stream"; flow:established,to_client; file_data; content:"EICAR",fast_pattern; metadata: service http2; sid:4; )
+alert tcp ( msg:"fast pattern file data match on HTTP/2 only"; file_data; content:"EICAR",fast_pattern; service: http2; sid:4; )
 ]]
 normalizer = {}
 
