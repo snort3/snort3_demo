ea152b3edc2826fc2f952358927b78efe7eb5f11
Katura Harvey <katharve@cisco.com>
2020-09-15 10:28:03 -0400

    codecs: update vlan test

--- tests/codecs/vlan/snort.lua
+++ tests/codecs/vlan/snort.lua
@@ -1,12 +1,3 @@
-
-dir = os.getenv('SNORT_LUA_PATH')
-
-if ( not dir ) then
-    dir = '.'
-end
-
-dofile(dir .. '/snort_defaults.lua')
-
 local_rules =
 [[
 alert icmp ( msg:"PROTOCOL-ICMP Echo Reply"; icode:0; itype:0; metadata:ruleset community; sid:99999; rev:1; )
