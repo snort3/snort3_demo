de11500d2792a525487d0820c518c879d5cb33e2
russ <rucombs@cisco.com>
2022-04-10 13:10:06 -0400

    service: fix rules to use service correctly

--- tests/ips_options/service/snort.lua
+++ tests/ips_options/service/snort.lua
@@ -30,14 +30,12 @@ dce_smb = { }
 
 local_rules =
 [[
-alert tcp any any -> any [445,139] ( msg:"NetshareEnum client alert"; dce_iface:uuid 4b324fc8-1670-01d3-1278-5a47bf6ee188; dce_opnum:"15"; dce_stub_data; content:"1|00|9|00|2|00|.|00|1|00|6|00|8",distance 16,within 13; sid:1; )
-alert tcp any [445,139] -> any any ( msg:"NetshareEnum server alert"; dce_iface:uuid 4b324fc8-1670-01d3-1278-5a47bf6ee188; dce_opnum:"15"; dce_stub_data; sid:2; )
-alert ( msg:"DCE2_EVENT__SMB_DCNT_ZERO"; sid:48; gid:133; rev:1; metadata:rule-type preproc,service dcerpc; )
+alert tcp any any -> any 12345 ( msg:"NetshareEnum client alert"; dce_iface:uuid 4b324fc8-1670-01d3-1278-5a47bf6ee188; dce_opnum:"15"; dce_stub_data; content:"1|00|9|00|2|00|.|00|1|00|6|00|8",distance 16,within 13; service:netbios-ssn; sid:1; )
+
+alert netbios-ssn any [445,139] -> any any ( msg:"NetshareEnum server alert"; dce_iface:uuid 4b324fc8-1670-01d3-1278-5a47bf6ee188; dce_opnum:"15"; dce_stub_data; sid:2; )
+
+alert ( msg:"DCE2_EVENT__SMB_DCNT_ZERO"; sid:48; gid:133; rev:1; )
 ]]
 
-ips =
-{
-    rules = local_rules,
-}
-
+ips = { rules = local_rules }
 
