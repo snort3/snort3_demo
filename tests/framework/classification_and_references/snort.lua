d4ff1028ff923e9013ac54e3bc2986f40c7c7a15
russ <rucombs@cisco.com>
2022-04-11 17:01:13 -0400

    ips: change Snort 2 style metadata:service foo to Snort 3 service:foo

--- tests/framework/classification_and_references/snort.lua
+++ tests/framework/classification_and_references/snort.lua
@@ -18,7 +18,7 @@ classifications = default_classifications
 
 local_rules = 
 [[
-alert udp $EXTERNAL_NET any -> $HOME_NET 5060 ( msg:"PROTOCOL-VOIP Digium Asterisk invite malformed SDP denial of service attempt"; flow:to_server; sip_method:invite; sip_header; content:"INVITE",depth 6,nocase; content:"INVITE",distance 0,nocase; sip_body; content:"c=IN IP",nocase; content:"c=IN IP",distance 0,nocase; byte_test:10,>,255,1,relative,string,dec; metadata:service sip; reference:bugtraq,23031; reference:cve,2007-1561; classtype:attempted-dos; sid:23966; rev:4; )
+alert udp $EXTERNAL_NET any -> $HOME_NET 5060 ( msg:"PROTOCOL-VOIP Digium Asterisk invite malformed SDP denial of service attempt"; flow:to_server; sip_method:invite; sip_header; content:"INVITE",depth 6,nocase; content:"INVITE",distance 0,nocase; sip_body; content:"c=IN IP",nocase; content:"c=IN IP",distance 0,nocase; byte_test:10,>,255,1,relative,string,dec; service:sip; reference:bugtraq,23031; reference:cve,2007-1561; classtype:attempted-dos; sid:23966; rev:4; )
 
 alert udp $EXTERNAL_NET any -> $HOME_NET 5060 ( msg:"invite"; flow:to_server; sip_method:invite; sip_header; content:"INVITE"; )
 ]]
