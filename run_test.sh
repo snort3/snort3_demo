04b95472a05d7ebfab96290e980a06282b67a992
Victor Roemer <victor@badsec.org>
2023-07-27 01:29:03 +0000

    snort3_demo: Refactor run_test.sh script
    This commit refactors the snort3_demo repository so that it can be executed
    somewhat more portably, and ideally feels a bit more comfortable to the user.
    
    These changes make one big assumption, namely:
    
     * snort and libdaq are installed to the default PREFIX directory
       (/usr/local).
    
       With this assumption made, the user is no longer responsible for
       specifying the install location for "snorty", i.e., `argv[1]` will
       now specify the path of tests to execute.
    
       NOTICE: If necessary, I will provide an override in the form of an
    	   environment variable to restore specifying an alternate
    	   install PREFIX.
    
    Now argv[1] specifices the path to the "top" directory to search for,
    and run tests from. The following commands are now all valid:
    
     * ./run_tests.sh tests/ips_options
    
        Runs all tests under "tests/ips_options/*"
    
     * ./run_tests.sh tests/ips_options/so_and_soid
    
        Runs the singular test under "tests/ips_options/so_and_soid/"
    
     * ./run_tests.sh tests/ips_options/so_and_soid/test.bats
    
        Identical to the previous exmaple, runs the singular test under
        "tests/ips_options/so_and_soid/"
    
     * ./run_tests.sh /root/alternate-path/some-private/integration-tests/
    
        Runs all the tests under the user specified external directory.
    
    The last major refactor to the script is the way which tests are
    "searched and exected". Previously, the run_tests.sh used `find` to
    create a singular long command line argument of "directories" containing
    the "tests.bat" script, passing it as an argument to a loop defined in
    the same file.
    
    The script is now chagned to use `find -exec run_one`, which is
    functionally equivalent, but is now future proofed from maxing out
    command line arguments, or size of a command line in the given shell.
    
    NOTICE: Acceptance of this PR will surely result in further refactorying
            to make the environment more portable still.

--- run_test.sh
+++ run_test.sh
@@ -1,45 +1,44 @@
 #!/usr/bin/env bash
-#$1 -- directory where snort is installed
-if [ $# -lt 1 ] ; then
-    printf "usage: run_test.sh <snort_install_dir>"
-    exit -1
+
+# Let the user specify an alternate directory to run test from
+# This could be within the same source tree, or external.
+TOPDIR="."
+if [ -n "$1" ]
+then TOPDIR="$1"
 fi
 
-[ -d "$SNORT3_DAQ_DIR" ] && daq_dir="--daq-dir $SNORT3_DAQ_DIR"
+if [[ -f "$TOPDIR" ]]
+then TOPDIR=$(dirname "$TOPDIR")
+fi
+
+# Assume user installed to the default location ("/usr/local")
+# TODO Deviation from this should pass as environment variable (`PREFIX="x/y/z" ./run_tests`).
+export PREFIX="/usr/local"
+export DAQ_DIR="$PREFIX/lib/daq:$PREFIX/lib/snort/daq:$PREFIX/lib/snort/daq/extra"
 
 # global args (add any needed here for your environment)
 args="-H -U"
 
 # setup environnement
-export snorty_path=$1
-export snort="$snorty_path/bin/snort $daq_dir $args"
-export snort2lua="$snorty_path/bin/snort2lua"
+export snort="$PREFIX/bin/snort --daq-dir $DAQ_DIR $args"
+export snort2lua="$PREFIX/bin/snort2lua"
 
-export LUA_PATH=$snorty_path/include/snort/lua/\?.lua\;\;
-export SNORT_LUA_PATH=$snorty_path/etc/snort/
+export LUA_PATH=$PREFIX/include/snort/lua/\?.lua\;\;
+export SNORT_LUA_PATH=$PREFIX/etc/snort/
 export PATH=$PATH:$(pwd)/bin
 
-if [ -d "${snorty_path}/lib64" ]; then
-    libs=${snorty_path}/lib64
-else
-    libs=${snorty_path}/lib
+# FIXIT: Figure out if libdaq install honors lib64 vs lib the same way as snort3 
+if [ -d "${PREFIX}/lib64" ]
+then LIBDIR="${PREFIX}/lib64"
+else LIBDIR="${PREFIX}/lib"
 fi
 
-export PKG_CONFIG_PATH=$libs/pkgconfig:$PKG_CONFIG_PATH
-export SNORT_DAQ_LIBS=$libs/snort/daq:$libs/snort/daq/extra
-export SNORT_PLUGINS=$libs/snort/plugins
+export PKG_CONFIG_PATH="$LIBDIR/pkgconfig:$PKG_CONFIG_PATH"
+export SNORT_PLUGINS="$LIBDIR/snort/plugins"
 
-tests=$2
-[ "$tests" ] || tests=`find . -name \*.bats`
-
-# run tests for all .bats
-for t in $tests ; do
-    printf "\n# $t:\n"
-    pushd . &>/dev/null
-    cd `dirname $t`
-    bats -t .
-    popd &> /dev/null
-    done | tee demo_result.log
+# Find will locate all the "test.bats" files under the specified TOPDIR.
+find "$TOPDIR" -name "test.bats" -type f -exec ./run_one \{} \; \
+	| tee demo_result.log
 
 # calculate stats
 pass=$(grep "^ok" -c demo_result.log)
@@ -51,9 +50,6 @@ total=$(($pass + $fail + $skip))
 printf "\n"
 printf "Total = $total, Pass = $pass, Fail = $fail, Skip = $skip\n" | tee demo_summary.log
 
-if [ $fail -ne 0 ]; then
-    exit 1;
-else
-    exit 0;
+if [ $fail -ne 0 ]
+then exit 1
 fi
-
