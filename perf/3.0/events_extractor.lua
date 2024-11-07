7aee573fccd28224f0dfc4116a0b1589e98a4314
Oleksii Shumeiko -X (oshumeik - SOFTSERVE INC at Cisco) <oshumeik@cisco.com>
2024-11-07 16:09:33 +0000

    Pull request #260: Add FTP proto to extractor test
    Merge in SNORT/snort3_demo from ~OSHUMEIK/snort3_demo:extractor_ftp to master
    
    Squashed commit of the following:
    
    commit 1693b71c1e5fd8d2f2de1cbe0bd89c9786d62697
    Author: Oleksii Shumeiko <oshumeik@cisco.com>
    Date:   Tue Nov 5 12:57:09 2024 +0200
    
        perf: add FTP proto to extractor test

--- perf/3.0/events_extractor.lua
+++ perf/3.0/events_extractor.lua
@@ -13,5 +13,12 @@ extractor =
                 'method, host, uri, user_agent, referrer, origin, version,'..
                 'status_code, status_msg, trans_depth'
         },
+        {
+            service = 'ftp',
+            on_events = 'request, response, eot',
+            fields = 'ts, id.orig_h, id.orig_p, id.resp_h, id.resp_p,'..
+                'uid, command, arg, user, reply_code, reply_msg, file_size,'..
+                'data_channel.passive, data_channel.orig_h, data_channel.resp_h, data_channel.resp_p'
+        },
     }
 }
