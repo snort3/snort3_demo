7b6abdf29a996bbe65dc07c5cd59a4324d95b777
Anna Norokh -X (anorokh - SOFTSERVE INC at Cisco) <anorokh@cisco.com>
2025-05-12 11:32:35 +0000

    Pull request #281: extractor: extend perf configuration with weird
    Merge in SNORT/snort3_demo from ~ANOROKH/snort3_demo:extr_detection to master
    
    Squashed commit of the following:
    
    commit c4a33153d9f023d8862bb37e16048eb30a353787
    Author: anorokh <anorokh@cisco.com>
    Date:   Tue May 6 16:54:36 2025 +0300
    
        extractor: extend config with all available fields
    
    commit 3641a94d09c27ee36ff9a70cd1035dd583db0123
    Author: anorokh <anorokh@cisco.com>
    Date:   Wed Apr 2 23:45:40 2025 +0300
    
        extractor: extend perf configuration with weird

--- perf/3.0/events_extractor.lua
+++ perf/3.0/events_extractor.lua
@@ -5,34 +5,43 @@ std_connector = { }
 extractor =
 {
     formatting = 'csv',
-    connector = "stdout",
+    connector = 'stdout',
     protocols =
     {
         {
             service = 'http',
             on_events = 'eot',
-            fields = 'ts, id.orig_h, id.orig_p, id.resp_h, id.resp_p,'..
+            fields = 'ts, uid, id.orig_h, id.orig_p, id.resp_h, id.resp_p, pkt_num, tenant_id,'..
                 'method, host, uri, user_agent, referrer, origin, version,'..
-                'status_code, status_msg, trans_depth'
+                'status_code, status_msg, trans_depth, request_body_len, response_body_len,'..
+                'info_code, info_msg, proxied, orig_filenames, resp_filenames,'..
+                'orig_mime_types, resp_mime_types'
         },
         {
             service = 'ftp',
             on_events = 'request, response, eot',
-            fields = 'ts, id.orig_h, id.orig_p, id.resp_h, id.resp_p,'..
+            fields = 'ts, uid, id.orig_h, id.orig_p, id.resp_h, id.resp_p, pkt_num, tenant_id,'..
                 'uid, command, arg, user, reply_code, reply_msg, file_size,'..
                 'data_channel.passive, data_channel.orig_h, data_channel.resp_h, data_channel.resp_p'
         },
         {
             service = 'conn',
             on_events = 'eof',
-            fields = 'ts, uid, id.orig_h, id.orig_p, id.resp_h, id.resp_p,'..
+            fields = 'ts, uid, id.orig_h, id.orig_p, id.resp_h, id.resp_p, pkt_num, tenant_id,'..
                 'duration, proto, service, orig_pkts, resp_pkts'
         },
         {
             service = 'dns',
             on_events = 'response',
-            fields = 'ts,uid,id.orig_h,id.orig_p,id.resp_h,id.resp_p,proto,trans_id,query,qclass,qclass_name,' ..
-                     'qtype,qtype_name,rcode,rcode_name,AA,TC,RD,RA,Z,answers,TTLs,rejected,auth,addl'
-        }
+            fields = 'ts, uid, id.orig_h, id.orig_p, id.resp_h, id.resp_p, pkt_num, tenant_id,'..
+                'proto, trans_id, query, qclass, qclass_name, qtype, qtype_name,' ..
+                'rcode, rcode_name, AA, TC, RD, RA, Z, answers, TTLs, rejected, auth, addl'
+        },
+        {
+            service = 'weird',
+            on_events = 'builtin',
+            fields = 'ts, uid, id.orig_h, id.orig_p, id.resp_h, id.resp_p, pkt_num, tenant_id,'..
+                'sid, gid, msg, proto, source'
+        },
     }
 }
