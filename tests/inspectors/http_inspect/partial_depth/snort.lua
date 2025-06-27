f121288a3fa89b62da3f7614d7ab6445894a4642
Adrian Mamolea (admamole) <admamole@cisco.com>
2025-06-27 13:55:35 +0000

    Pull request #287: http_inspect: add test for partial depth
    Merge in SNORT/snort3_demo from ~ADMAMOLE/snort3_demo:cl_demo to master
    
    Squashed commit of the following:
    
    commit 8d1078088661324e33d3e4341a3505615d9a8fac
    Author: Adrian Mamolea <admamole@cisco.com>
    Date:   Thu Jun 26 11:42:44 2025 -0400
    
        http_inspect: add test for partial depth

---
+++ tests/inspectors/http_inspect/partial_depth/snort.lua
@@ -0,0 +1,52 @@
+HOME_NET = 'any'
+EXTERNAL_NET = 'any'
+
+dir = os.getenv('SNORT_LUA_PATH')
+
+if ( not dir ) then
+    dir = ''
+end
+
+dofile(dir .. 'snort_defaults.lua')
+
+default_rules =
+[[
+block http ( flow:established,to_server; content:"tricky"; sid:1; rev:1; )
+]]
+
+http_inspect =
+{
+    partial_depth = 1000
+}
+
+event_queue =
+{
+    log = 16,
+    max_queue = 16
+}
+
+search_engine =
+{
+    max_queue_events = 16,
+}
+
+normalizer =
+{
+    tcp =
+    {
+        ips = true,
+    }
+}
+
+stream = {}
+
+stream_tcp = {}
+
+wizard = default_wizard
+
+ips =
+{
+    enable_builtin_rules = true,
+    rules = default_rules
+}
+
---
+++ tests/inspectors/http_inspect/partial_depth/test.bats
@@ -0,0 +1,18 @@
+#!/usr/bin/env bats
+
+PCAP="bad_content_length.pcap"
+CFG="snort.lua"
+OPTION="-q -Q -A csv --daq dump"
+
+@test "Partial depth - malware block in post request with bad content length" {
+    $snort -c $CFG -r $PCAP $OPTION > snort.out
+    diff expected snort.out
+    diff expected.pcap inline-out.pcap
+}
+
+teardown()
+{
+    rm -f snort.out
+    rm -f inline-out.pcap
+}
+
